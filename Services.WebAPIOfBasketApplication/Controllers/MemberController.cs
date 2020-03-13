e8cc863fd40f1557379addbc9c73b2e256499150
SINAN HAMZACEBI <DTSHAMZACEBI@digiturk.com.tr>
2020-03-13 14:58:08 +0300

    typeof(Members) icin gerekli servis fonksiyonlari yazildi

---
+++ Services.WebAPIOfBasketApplication/Controllers/MemberController.cs
@@ -0,0 +1,49 @@
+﻿#region Added Project References and General Usings
+using System;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Managers.ManagerOfBasketApplication.Abstracts;
+#endregion Added Project References and General Usings
+
+namespace Services.WebAPIOfBasketApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces(contentType: MediaTypeNames.Application.Json)]
+    public class MemberController : ControllerBase
+    {
+        #region Global Private Readonly Properties
+        private readonly IManagerOfMembers ManagerOfMembers;
+        #endregion Global Private Readonly Properties
+
+
+        #region Constructors
+        public MemberController(IManagerOfMembers managerOfMembers)
+        {
+            this.ManagerOfMembers = managerOfMembers;
+        }
+        #endregion Constructors
+
+
+        #region Public Service Functions
+
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(statusCode: StatusCodes.Status204NoContent)]
+        [HttpPost(Name = "{memberId:guid}"), Route(template: "fetch-any-member-by-member-id")]
+        public IActionResult FetchAnyMemberByMemberId([FromBody]Guid memberId)
+        {
+            var resultFromFetchAnyMemberById = this.ManagerOfMembers
+                                                   .FetchMemberById(idOfMember: memberId);
+            if (resultFromFetchAnyMemberById.SuccessInformation.IsResultSuccessful)
+            {
+                return StatusCode(statusCode: StatusCodes.Status204NoContent,
+                                  value: resultFromFetchAnyMemberById);
+            }
+            return StatusCode(statusCode: StatusCodes.Status204NoContent,
+                  value: resultFromFetchAnyMemberById.SuccessInformation);
+        }
+
+        #endregion Public Service Functions
+    }
+}
