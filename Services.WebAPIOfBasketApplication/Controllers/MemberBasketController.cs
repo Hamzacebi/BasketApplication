a9c06f4ce8796fae0549fc8cc7021cf50062f1d3
Sinan Hamzaçebi <sinan.hamzacebi@gmail.com>
2020-04-27 14:31:22 +0300

    Sepet icin gerekli fonksiyonlar eklendi

--- Services.WebAPIOfBasketApplication/Controllers/MemberBasketController.cs
+++ Services.WebAPIOfBasketApplication/Controllers/MemberBasketController.cs
@@ -4,6 +4,7 @@ using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Managers.ManagerOfBasketApplication.Abstracts;
+using Models.OtherNeccesaryModelsOfBasketApplication.ModelsOfWebAPI.WebAPIModelsOfMemberBasket;
 #endregion Added Project References and General Usings
 
 namespace Services.WebAPIOfBasketApplication.Controllers
@@ -28,7 +29,7 @@ namespace Services.WebAPIOfBasketApplication.Controllers
 
         #region Public Service Functions
 
-        [ProducesResponseType(statusCode: StatusCodes.Status200OK)] 
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
         [HttpPost(Name = "{idOfBasketOwnerId:guid}"), Route(template: "buy-products-in-cart-by-basket-owner-id")]
         public IActionResult BuyProductsInCart([FromBody]Guid idOfBasketOwnerId)
         {
@@ -36,7 +37,39 @@ namespace Services.WebAPIOfBasketApplication.Controllers
                                                   .BuyProductsInCart(idOfBasketOwnerId: idOfBasketOwnerId);
 
             return StatusCode(statusCode: StatusCodes.Status200OK,
-                                  value: resultFromBuyProductsInCart);
+                              value: resultFromBuyProductsInCart);
+        }
+
+
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [HttpPost(Name = "{idOfMemberBasket:guid}"), Route(template: "delete-any-product-in-the-basket")]
+        public IActionResult DeleteAnyProductInTheBasket([FromBody]Guid idOfMemberBasket)
+        {
+            var resultFromDeleteAnyProductInTheBasket = this.ManagerOfMemberBaskets
+                                                            .DeleteAnyProductInTheBasket(idOfMemberBasket: idOfMemberBasket);
+            return StatusCode(statusCode: StatusCodes.Status200OK,
+                              value: resultFromDeleteAnyProductInTheBasket);
+        }
+
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [HttpPost, Route(template: "add-product-to-cart")]
+        public IActionResult AddProductToCart(WebAPIModelOfInsertMemberBasket productToAddedToBasket)
+        {
+            var resultFromAddProductToCart = this.ManagerOfMemberBaskets
+                                                 .AddProductToCart(productToBeAddedToCart: productToAddedToBasket);
+
+            return StatusCode(statusCode: StatusCodes.Status200OK,
+                              value: resultFromAddProductToCart);
+        }
+
+
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        [HttpPost(Name = "memberId:guid"), Route(template: "fetch-all-products-owned-by-the-member")]
+        public IActionResult FetchAllProductsOwnedByTheMember([FromBody]Guid memberId)
+        {
+            return StatusCode(statusCode: StatusCodes.Status200OK,
+                              value: this.ManagerOfMemberBaskets
+                                         .FetchAllProductsOwnedByTheMember(memberId));
         }
 
         #endregion Public Service Functions
